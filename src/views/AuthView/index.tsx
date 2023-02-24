14687a405c54049a10515b851872e3b8304c57bb
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2023-02-24 19:50:36 +0300

    configured eslint

--- src/views/AuthView/index.tsx
+++ src/views/AuthView/index.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 
 const AuthView = () => {
-    return <View>
-        <Text>AuthScreen</Text>
-    </View>;
+    return (
+        <View>
+            <Text>AuthScreen</Text>
+        </View>
+    );
 };
 
 export default AuthView;
