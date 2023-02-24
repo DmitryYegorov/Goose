f17f88646cefa8ccee1c8ce16415f5a01e9521c0
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2023-02-24 19:37:17 +0300

    init project

---
+++ src/views/AuthView/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const AuthView = () => {
+    return <View>
+        <Text>AuthScreen</Text>
+    </View>;
+};
+
+export default AuthView;
